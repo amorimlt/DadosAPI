7201761c79ab5af102cc854ec228474ac35800c5
amorimlt <julianeamorim.lt@gmail.com>
2022-08-11 22:38:35 -0300

    aplicativo de rolagem de dado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/dado/d{NumeroFaces}", (
+    [FromRoute]int NumeroFaces
+    ) => 
+    {
+        if (NumeroFaces <= 0)
+        {
+            return Results.BadRequest(new { mensagem = "Somente dados com pelo menos uma face"});
+        }
+    int face = RandomNumberGenerator.GetInt32(1, NumeroFaces + 1);
+    return Results.Ok(new {dado = $"d{NumeroFaces}", rolagem = face});
+    });
+
+app.Run();
